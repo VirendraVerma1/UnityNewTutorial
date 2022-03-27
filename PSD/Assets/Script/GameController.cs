4b58520cd5a4b476839b022c04ddebe4710d7225
Virendra <virendra@pacdevelopers.com>
2022-03-28 00:16:52 +0530

    uodate game over, object pannel

--- PSD/Assets/Script/GameController.cs
+++ PSD/Assets/Script/GameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -16,14 +17,16 @@ public class GameController : MonoBehaviour
     public GameObject[] starLife;
     public Sprite[] predifinedShapeUI;
     public Image predefinedShapeUIContainer;
+    public bool isPause = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        saveload.playerScore = 0;
-        saveload.Save();
-        saveload.Load();
-        var data = saveload.playerScore;
+        isPause = false;
+        //saveload.playerScore = 0;
+        //saveload.Save();
+        //saveload.Load();
+        //var data = saveload.playerScore;
         initializeAllPanels();
     }
 
@@ -31,6 +34,8 @@ public class GameController : MonoBehaviour
     {
         while(!isGameLose)
         {
+            isPause = true;
+            yield return new WaitForSeconds(3);
             int randomShape = Random.RandomRange(0, predefinedShape.Length);
             foreach(GameObject g in predefinedShape)
             {
@@ -39,7 +44,16 @@ public class GameController : MonoBehaviour
             predefinedShape[randomShape].SetActive(true);
             validShape = predefinedShape[randomShape].GetComponent<ShapeController>().shapeName;
             predefinedShapeUIContainer.sprite = predifinedShapeUI[randomShape];
-            yield return new WaitForSeconds(5);
+            validObjectImage.sprite = predifinedShapeUI[randomShape];
+            onValidObjectChange();
+            
+            yield return new WaitForSeconds(3);
+            deactivateAllPanels();
+            isPause = false;
+            yield return new WaitForSeconds(15);
+            //pause for a while
+            //show the changes object pannel
+            
         }
     }
 
@@ -47,14 +61,18 @@ public class GameController : MonoBehaviour
     {
         while(!isGameLose)
         {
-            float randomTime = Random.RandomRange(0.2f,0.7f);
-            yield return new WaitForSeconds(randomTime);
-            float randomDrag = Random.RandomRange(0.5f,3);
-            int randomPositions = Random.RandomRange(0, positions.Length);
-            int randomShapes = Random.RandomRange(0, shapes.Length);
-            GameObject go = Instantiate(shapes[randomShapes], positions[randomPositions].position, positions[randomPositions].rotation);
-            go.GetComponent<Rigidbody>().drag = randomDrag;
-            Destroy(go, 5);
+            if (!isPause)
+            {
+                float randomTime = Random.RandomRange(0.2f, 0.7f);
+                yield return new WaitForSeconds(randomTime);
+                float randomDrag = Random.RandomRange(0.5f, 3);
+                int randomPositions = Random.RandomRange(0, positions.Length);
+                int randomShapes = Random.RandomRange(0, shapes.Length);
+                GameObject go = Instantiate(shapes[randomShapes], positions[randomPositions].position, positions[randomPositions].rotation);
+                go.GetComponent<Rigidbody>().drag = randomDrag;
+                Destroy(go, 5);
+            }
+            yield return new WaitForSeconds(0.5f);
         }
     }
 
@@ -76,16 +94,19 @@ public class GameController : MonoBehaviour
                     if(hit.collider.gameObject.GetComponent<ShapeController>().shapeName == validShape)
                     {
                         score++;
+                        FindObjectOfType<AudioManager>().Play("CorrectTouch");
                         if(score%5==0 && life < 5)
                         {
                             starLife[life].SetActive(true);
                             life++;
                         }
                         scoreText.text = score.ToString();
+                        gameOverScoreText.text = score.ToString();
                     }
                     else
                     {
                         life--;
+                        FindObjectOfType<AudioManager>().Play("IncorrectTouch");
                         starLife[life].SetActive(false);
                     }
                     if(life<=0)
@@ -115,6 +136,8 @@ public class GameController : MonoBehaviour
     [Header("UI")]
     public GameObject startPanel;
     public GameObject gameOverPanel;
+    public GameObject validObjectChange;
+    public Image validObjectImage;
     public void initializeAllPanels()
     {
         deactivateAllPanels();
@@ -133,12 +156,29 @@ public class GameController : MonoBehaviour
     {
         deactivateAllPanels();
         gameOverPanel.SetActive(true);
+        FindObjectOfType<AudioManager>().Play("GameOverSound");
     }
 
     public void deactivateAllPanels()
     {
         startPanel.SetActive(false);
         gameOverPanel.SetActive(false);
+        validObjectChange.SetActive(false);
 
     }
+
+    public void onRestartButtonClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void onValidObjectChange()
+    {
+        deactivateAllPanels();
+        validObjectChange.SetActive(true);
+    }
+
+    [Header("gameOver")]
+    public Text gameOverScoreText;
+
 }
